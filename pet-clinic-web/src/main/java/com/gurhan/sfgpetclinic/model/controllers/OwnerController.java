7db00949da0e31a66cbfb94d53c76da6dae841d9
Gürhan Küçük <gurhan.kucuk@yandex.com>
2019-08-17 12:25:20 +0300

    Added owner page.

---
+++ pet-clinic-web/src/main/java/com/gurhan/sfgpetclinic/model/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.gurhan.sfgpetclinic.model.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String listOwner() {
+        return "owner/index";
+    }
+}
