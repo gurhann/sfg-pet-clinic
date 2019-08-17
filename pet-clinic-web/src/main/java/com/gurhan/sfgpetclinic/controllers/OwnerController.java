288e48ccc2da688c39adc01e650bde123454f347
Gürhan Küçük <gurhan.kucuk@yandex.com>
2019-08-17 17:21:15 +0300

    added  owner list and updated controllers.

--- pet-clinic-web/src/main/java/com/gurhan/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/gurhan/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.gurhan.sfgpetclinic.controllers;
 
+import com.gurhan.sfgpetclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
