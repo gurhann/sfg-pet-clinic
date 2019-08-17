2625df0c5b49282de47c0f5fdf76b9063a1cc9a9
Gürhan Küçük <gurhan.kucuk@yandex.com>
2019-08-17 12:03:06 +0300

    Added map ser service implemantations.

---
+++ pet-clinic-data/src/main/java/com/gurhan/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.gurhan.sfgpetclinic.service.map;
+
+import com.gurhan.sfgpetclinic.model.Owner;
+import com.gurhan.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
